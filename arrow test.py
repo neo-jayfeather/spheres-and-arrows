262de801ca8453754503e90d2762c36adee85777
neo-jayfeather <156867325+neo-jayfeather@users.noreply.github.com>
2024-05-29 16:42:54 -0400

    updated spheres and arrows
    the mesh concat does not work between spheres and arrows

--- arrow test.py
+++ arrow test.py
@@ -1,16 +1,7 @@
 import trimesh
 import numpy as np
 
-direction_map = {
-  "X" : [2,1,0],
-  "Y" : [0,2,1],
-  "Z" : [0,1,2]
-}
-multi_map = {
-  "-X" : [-1,1,1],
-  "-Y" : [1,-1,1],
-  "-Z" : [1,1,-1]
-}
+
 def concatMesh(mesh1, mesh2, meshOut):
   # takes mesh 1 and, concats their vertices, then concats their triangles
   # also makes the triangle mesh indexing start from the last index in the first mesh
@@ -21,8 +12,8 @@ def concatMesh(mesh1, mesh2, meshOut):
   meshOut.vertices = np.concatenate((mesh1.vertices, mesh2.vertices), axis=0)
 class emptyMesh:
   def __init__(self):
-    self.faces = []
-    self.vertices = []
+    self.faces = [[0,0,0]]
+    self.vertices = [[0,0,0]]
 
 class Arrow:
   def __init__(self, bodySize, coords, arrowSize, upDir):
@@ -31,6 +22,17 @@ class Arrow:
     self.arrowLength, self.arrowRadius = arrowSize
     self.direction = upDir
   def create_mesh(self):
+    direction_map = {
+      "X" : [2,1,0],
+      "Y" : [0,2,1],
+      "Z" : [0,1,2]
+    }
+    multi_map = {
+      "-X" : [-1,1,1],
+      "-Y" : [1,-1,1],
+      "-Z" : [1,1,-1]
+    }
+  
     self.cylinder = trimesh.creation.cylinder(radius = self.bodyRadius, height = self.bodyLength, sections = 256)
     self.cone = trimesh.creation.cone(radius = self.arrowRadius, height = self.arrowLength, sections = 256)
     self.mesh = emptyMesh()
@@ -39,35 +41,25 @@ class Arrow:
     concatMesh(self.cylinder,self.cone,self.mesh)
     self.vertices = self.mesh.vertices
     self.faces = self.mesh.faces
-    
     multiChange = multi_map.get(self.direction, [1,1,1])
     self.direction = self.direction.split('-')[len(self.direction.split('-'))-1]
     dirChange = direction_map.get(self.direction)
-
-    for x in range(len(self.vertices)):
-      #a = self.vertices[x]
+    for x in range(len(self.vertices)): # need better rotation algorithm but this works!
       self.vertices[x] = [self.vertices[x][dirChange[0]],self.vertices[x][dirChange[1]],self.vertices[x][dirChange[2]]]
     self.vertices *= multiChange
-    
-arrow = Arrow([30,5],[0,0,0],[7,7],'-Z')
-arrow1 = Arrow([50,4],[0,0,0],[10,10],"Z")
-arrow2 = Arrow([30,5],[0,0,0],[7,7],'X')
-arrow3 = Arrow([30,5],[0,0,0],[7,7],'-X')
-arrow4 = Arrow([30,5],[0,0,0],[7,7],'Y')
-arrow5 = Arrow([30,5],[0,0,0],[7,7],'-Y')
-arrow.create_mesh()
-arrow1.create_mesh()
-arrow2.create_mesh()
-arrow3.create_mesh()
-arrow4.create_mesh()
-arrow5.create_mesh()
-concatMesh(arrow,arrow1,arrow)
-concatMesh(arrow,arrow2,arrow)
-concatMesh(arrow,arrow3,arrow)
-concatMesh(arrow,arrow4,arrow)
-concatMesh(arrow,arrow5,arrow)
+arrows =  [
+  Arrow([30,5],[0,0,0],[7,7],'-Z'),
+  Arrow([30,5],[0,0,0],[7,7],"Z"),
+  Arrow([30,5],[0,0,0],[7,7],'X'),
+  Arrow([30,5],[0,0,0],[7,7],'-X'),
+  Arrow([30,5],[0,0,0],[7,7],'Y'),
+  Arrow([30,5],[0,0,0],[7,7],'-Y'),
+  emptyMesh()
+]
+totalArrows = len(arrows)-1
+for x in range(totalArrows):
+  arrows[x].create_mesh()
+  concatMesh(arrows[totalArrows],arrows[x], arrows[totalArrows])
 
-for x in range(len(arrow.vertices)):
-  arrow.vertices[x] = [arrow.vertices[x][2],arrow.vertices[x][0],arrow.vertices[x][1]]
-output = trimesh.Trimesh(vertices = arrow.vertices, faces = arrow.faces)
+output = trimesh.Trimesh(vertices = arrows[totalArrows].vertices, faces = arrows[totalArrows].faces)
 output.export("C:\\users\\ncneo\\desktop\\cylinder.stl","stl")
--- spheres and arrows.py
+++ spheres and arrows.py
@@ -11,8 +11,8 @@ def concatMesh(mesh1, mesh2, meshOut):
   meshOut.vertices = np.concatenate((mesh1.vertices, mesh2.vertices), axis=0)
 class emptyMesh:
   def __init__(self):
-    self.faces = []
-    self.vertices = []
+    self.faces = [[0,0,0]]
+    self.vertices = [[0,0,0]]
 class Sphere:
   def __init__(self, subDivs, radius, position):
     self.subDivs = subDivs
@@ -30,36 +30,69 @@ class Arrow:
     self.arrowLength, self.arrowRadius = arrowSize
     self.direction = upDir
   def create_mesh(self):
+    direction_map = {
+      "X" : [2,1,0],
+      "Y" : [0,2,1],
+      "Z" : [0,1,2]
+    }
+    multi_map = {
+      "-X" : [-1,1,1],
+      "-Y" : [1,-1,1],
+      "-Z" : [1,1,-1]
+    }
+  
     self.cylinder = trimesh.creation.cylinder(radius = self.bodyRadius, height = self.bodyLength, sections = 256)
-    self.cone = trimesh.creation.cone(radius = self.arrowRadius, height = self.arrowLength)
+    self.cone = trimesh.creation.cone(radius = self.arrowRadius, height = self.arrowLength, sections = 256)
     self.mesh = emptyMesh()
+    self.cone.vertices += [0,0,self.bodyLength]
+    self.cylinder.vertices += [0,0,self.bodyLength/2]
     concatMesh(self.cylinder,self.cone,self.mesh)
+    self.vertices = self.mesh.vertices
+    self.faces = self.mesh.faces
+    multiChange = multi_map.get(self.direction, [1,1,1])
+    self.direction = self.direction.split('-')[len(self.direction.split('-'))-1]
+    dirChange = direction_map.get(self.direction)
+    for x in range(len(self.vertices)): # need better rotation algorithm but this works!
+      self.vertices[x] = [self.vertices[x][dirChange[0]],self.vertices[x][dirChange[1]],self.vertices[x][dirChange[2]]]
+    self.vertices *= multiChange
 
 
-def main():
-  # Sphere parameters
-  subDivs = 4
-  radius = 10
+#arrows
+arrows =  [
+  Arrow([30,5],[0,0,0],[7,7],'-Z'),
+  Arrow([30,5],[0,0,0],[7,7],"Z"),
+  Arrow([30,5],[0,0,0],[7,7],'X'),
+  Arrow([30,5],[0,0,0],[7,7],'-X'),
+  Arrow([30,5],[0,0,0],[7,7],'Y'),
+  Arrow([30,5],[0,0,0],[7,7],'-Y'),
+  emptyMesh()
+]
+totalArrows = len(arrows)-1
+for x in range(totalArrows):
+  arrows[x].create_mesh()
+  concatMesh(arrows[totalArrows],arrows[x], arrows[totalArrows])
+output = trimesh.Trimesh(vertices = arrows[totalArrows].vertices, faces = arrows[totalArrows].faces)
+#output.export("C:\\users\\ncneo\\desktop\\cylinder.stl","stl")
 
-  # Create sphere objects
-  sphere_left = Sphere(subDivs, radius, [-50, 0, 0])
-  sphere_right = Sphere(subDivs, radius, [50, 0, 0])
-  sphere_middle = Sphere(subDivs, radius, [0,0,0])
-  # Create meshes for each sphere (vertices only)
-  sphere_left.create_mesh()
-  sphere_right.create_mesh()
-  sphere_middle.create_mesh()
-  newMesh = emptyMesh()
-  concatMesh(sphere_left, sphere_right,  newMesh)
-  concatMesh(newMesh, sphere_middle, newMesh)
+# Sphere parameters
+subDivs = 4
+radius = 10
 
-  # Create the final mesh object
-  mesh = trimesh.Trimesh(vertices=newMesh.vertices, faces=newMesh.faces)
+# Create sphere objects
+sphere_left = Sphere(subDivs, radius, [-50, 0, 0])
+sphere_right = Sphere(subDivs, radius, [50, 0, 0])
+sphere_middle = Sphere(subDivs, radius, [0,0,0])
+# Create meshes for each sphere (vertices only)
+sphere_left.create_mesh()
+sphere_right.create_mesh()
+sphere_middle.create_mesh()
+newMesh = emptyMesh()
+concatMesh(sphere_left, sphere_right,  newMesh)
+concatMesh(newMesh, sphere_middle, newMesh)
+concatMesh(newMesh, arrows[totalArrows], newMesh)
+# Create the final mesh object
+mesh = trimesh.Trimesh(vertices=arrows[totalArrows].vertices, faces=arrows[totalArrows].faces)
+# Export the mesh as STL
+mesh.export("two_spheres_combined.stl")
 
-  # Export the mesh as STL
-  mesh.export("two_spheres_combined.stl")
-
-  print("Exported combined spheres to two_spheres_combined.stl")
-
-if __name__ == "__main__":
-  main()
+print("Exported combined spheres to two_spheres_combined.stl")
